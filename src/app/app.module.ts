7e67755fb99450c9b95873d6597228ae188706e8
wiloon <wiloon.wy@gmail.com>
2017-12-02 21:25:53 +0800

    heroes, details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import {AppComponent} from './app.component';
 import {HelloComponent} from './hello.component';
 import {HeroComponent} from './hero.component';
 import {HeroChildComponent} from './herochild.component';
 import {HttpClientModule} from '@angular/common/http';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
     HeroComponent,
-    HeroChildComponent
+    HeroChildComponent,
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
